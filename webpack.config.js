bf74d0e6913ff4e02ab571bc648436a0d64a82bc
Rochelle Yang Canale <chelle@cbo.me>
2020-03-04 15:21:09 +0800

    Modified webpack for entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
